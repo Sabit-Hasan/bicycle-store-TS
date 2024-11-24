c2c7fd86a36fa6543a05cc68787b86974a19ce86
Sabit-Hasan <hayshin.sabit@gmail.com>
2024-11-24 12:25:34 +0600

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Application } from 'express';
 import bicycleRoute from './app/modules/bicycle/bicycle.route';
 import orderRoute from './app/modules/order/order.router';
+import cors from 'cors';
 
 const app: Application = express();
 
 // middleware
 app.use(express.json());
+app.use(cors());
 
 // routes
 app.use('/api/products', bicycleRoute); // bicycle route
